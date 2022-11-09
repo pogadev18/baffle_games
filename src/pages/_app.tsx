bb8aa3a03e5173c3bbc5e86170519f5c69e98683
pogadev <pogadev18@gmail.com>
2022-11-09 19:35:41 +0200

    added moralis connection with metamask

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { createClient, configureChains, defaultChains, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { SessionProvider } from 'next-auth/react';
+
+import '../styles/globals.css'
+
+const {provider, webSocketProvider} = configureChains(defaultChains, [publicProvider()]);
+
+const client = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: true,
+});
+
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <WagmiConfig client={client}>
+      <SessionProvider session={pageProps.session} refetchInterval={0}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </WagmiConfig>
+  );
 }
+
+export default MyApp;
---
+++ src/pages/user.tsx
@@ -0,0 +1,32 @@
+import { getSession, GetSessionParams, signOut } from 'next-auth/react';
+import { InferGetServerSidePropsType } from "next";
+
+function User({user}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div>
+      <h4>User session:</h4>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <button onClick={() => signOut({redirect: true})}>Sign out</button>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context: GetSessionParams | undefined) {
+  const session = await getSession(context);
+
+  // redirect if not authenticated
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/signin',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {user: session.user},
+  };
+}
+
+export default User;
